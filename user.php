2717feea87a7f6a8610583d44c3cf788ebbc4b66
xy <1010873222@qq.com>
2017-08-30 23:48:06 +0800

    my homework1

--- user.php
+++ user.php
@@ -31,10 +31,6 @@ function json ($data = [], $msg = 'success', $code = 0, $exit = true) {
     }
 }
 
-$user_name = $_POST('user_name');
-$password = $_POST('password');
-$user_nameLen = strlen($user_name);
-$passwordLen = strlen($password);
 // 执行操作
 if ($action == 'register') {
     // 注册操作
@@ -44,6 +40,10 @@ if ($action == 'register') {
     $user_name = empty($_POST['user_name']) ? null : trim($_POST['user_name']);
     $password = empty($_POST['password']) ? null : trim($_POST['password']);
 
+    $user_name = $_POST($user_name);
+    $password = $_POST($password);
+    $user_nameLen = strlen($user_name);
+    $passwordLen = strlen($password);
 
     if ($user_nameLen = 0) {
         exit('用户名不能为空');
@@ -56,12 +56,12 @@ if ($action == 'register') {
     };
 
     //验证用户名是否已存在
-    $result = $db->query("select user_name from `users` where user_name='$user_name'");
+    $result = $db->query("select * from `users` where `user_name`='$user_name'");
     if ($result->fetchObject()) return json([], '用户名已被使用', -1);
 
     $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = $db->exec("insert into `users` values (null, '$user_name', '$password', null, null)");
+    $result = $db->exec("insert into `users` (`user_name`,`password`) values ('$user_name','$hash_password'");
     if ($result == 1) {
         return json([], '注册成功');
     } else{
@@ -78,16 +78,15 @@ if ($action = 'login'){
         exit('密码不能为空');
     }
 
-    $result = $db->query("select * from `users` where user_name='$user_name'");
-    $user = $result->fetchObject();
+    $result1= $db->query("select * from `users` where `user_name`='$user_name'");
+    $user = $result1->fetchObject();
     if (empty($user)) return json([],'用户名不存在');
     $hash_password = $user->password;
-    $user_id = $user->id;
 
-    $result = $db->query("select * from `users` where user_name ='$user_name' and password = '$password'");
+    $result = $db->query("select * from `users` where user_name ='$user_name' and password = '$hash_password'");
     $log = $result->fetchObject();
     if (empty($log)) return json([],'密码错误');
-    return json(['user_name = $user_name','password = $password'],'登陆成功');
+    return json(['user_name = $user_name','password = $hash_password'],'登陆成功');
 }
 //登陆 login
 //注册 register
