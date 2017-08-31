f384caa8fa13811881411ab6e64be40f84a65941
xy <1010873222@qq.com>
2017-08-31 20:35:48 +0800

    my homework1

--- user.php
+++ user.php
@@ -6,7 +6,7 @@
  * Time: 13:53
  */
 try{
-    $db = new PDO('mysql:host = localhost:dbname = blog','root','zfy5201314');
+    $db = new PDO('mysql:host=localhost;dbname=blog','root','zfy5201314');
 }catch (PDOException$error) {
     echo '链接数据库失败'.$error->getMessage();
     exit();
@@ -40,16 +40,10 @@ if ($action == 'register') {
     $user_name = empty($_POST['user_name']) ? null : trim($_POST['user_name']);
     $password = empty($_POST['password']) ? null : trim($_POST['password']);
 
-    $user_name = $_POST($user_name);
-    $password = $_POST($password);
     $user_nameLen = strlen($user_name);
     $passwordLen = strlen($password);
 
-    if ($user_nameLen = 0) {
-        exit('用户名不能为空');
-    } elseif ($passwordLen = 0) {
-        exit('密码不能为空');
-    } elseif ($user_nameLen < 6) {
+    if ($user_nameLen < 6) {
         exit('用户名长度不能小于6');
     } elseif ($passwordLen < 6) {
         exit('密码长度不能小于6');
@@ -61,32 +55,47 @@ if ($action == 'register') {
 
     $hash_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = $db->exec("insert into `users` (`user_name`,`password`) values ('$user_name','$hash_password'");
+    $result = $db->exec("insert into `users` (`user_name`,`password`) values ('$user_name','$hash_password')");
     if ($result == 1) {
         return json([], '注册成功');
     } else{
         return json([$db->errorInfo()], '注册失败', -1);
     }
+
 }
-if ($action = 'login'){
+if ($action = 'login') {
     $user_name = empty($_POST['user_name']) ? null : trim($_POST['user_name']);
     $password = empty($_POST['password']) ? null : trim($_POST['password']);
+    if (empty($user_name)) return json([], '用户名不能为空', -1);
+    if (empty($password)) return json([], '密码不能为空', -1);
 
-    if ($user_nameLen = 0) {
-        exit('用户名不能为空');
-    } elseif ($passwordLen = 0) {
-        exit('密码不能为空');
+    $result1 = $db->query("select * from `users` where `user_name`='$user_name'");
+    $log = $result1->fetchObject();
+    if (empty($log)) return json([], '用户名不存在');
+    $hash_password = $log->password;
+    $user_id = $log->id;
+
+    if (password_verify($password, $hash_password)) {
+        $token = password_hash($password, $hash_password);
+        $only = $db->query("update users set token = '$token' where id = '$user_id'");
+        if ($only < 1) return json([], 'token创建失败', -1);
+        return json(['user_name = $user_name', 'password = $hash_password'], '登陆成功');
+    }else {
+        return json([],'用户名或者密码错误');
+    }
+}
+if ($action = 'get_self_info'){
+    $is_login = get_login_status($db,$uid);
+    if (!$is_login) return json([],'未登录',-1);
+    $result= $db->query("select * from `user` where id = '$uid'");
+    if ($result){
+        $user = $result->fetch($db::FETCH_ASSOC);
+        unset($user['password'],$user['token']);
+        return json(['item'=>$user]);
+    }else{
+        return json([$db->errorInfo()],'查询失败',-1);
     }
 
-    $result1= $db->query("select * from `users` where `user_name`='$user_name'");
-    $user = $result1->fetchObject();
-    if (empty($user)) return json([],'用户名不存在');
-    $hash_password = $user->password;
-
-    $result = $db->query("select * from `users` where user_name ='$user_name' and password = '$hash_password'");
-    $log = $result->fetchObject();
-    if (empty($log)) return json([],'密码错误');
-    return json(['user_name = $user_name','password = $hash_password'],'登陆成功');
 }
 //登陆 login
 //注册 register
